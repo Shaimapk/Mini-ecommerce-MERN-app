c3b6d8b5348a134089e637b28f2f823f34f9293a
Shaimapk <shaimapks@gmail.com>
2026-07-06 09:27:38 +0530

    styling to cart page

--- frontend/src/pages/user/Cart.jsx
+++ frontend/src/pages/user/Cart.jsx
@@ -7,14 +7,30 @@ export default function Cart() {
     const {cartItems} = useSelector((state)=>state.cart)
 
   return (
-    <div>
-        {cartItems.map((item)=>(
-            <div key={item.id}>
-                <h3>{item.name}</h3>
-                <p>{item.price}</p>
-                <p>{item.quantity}</p>
+    <div className="w-full">
+        {cartItems.length >0 && 
+            <div>
+                <div className="flex my-5 mx-10 text-xl font-bold border-b p-4">
+                    <h2 className="w-1/2">Items</h2>
+                    <h2 className="w-1/4 text-center">Price</h2>
+                    <h2 className="w-1/4 text-center">Qty</h2>
+                </div>
+                {cartItems.map((item)=>(
+                    <div key={item.id} className="flex my-5 mx-10 border-b p-4 border-b-gray-300 shadow-black " >
+                        <h3 className="w-1/2">{item.name}</h3>
+                        <p className="w-1/4 text-center">{item.price}</p>
+                        <p className="w-1/4 text-center">{item.quantity}</p>
+                    </div>
+                ))}
             </div>
-        ))}
+        }
+
+        {cartItems.length === 0 && 
+            <div>
+                <p className="m-5 text-lg">Your cart is empty.</p>
+            </div>
+        }
+        
     </div>
   )
 }
